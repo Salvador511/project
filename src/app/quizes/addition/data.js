9fa056e4389e2eb19823ba82da37c3de5326e658
Salvador511 <salvadororozcogon511@gmail.com>
2023-11-09 16:23:07 -0700

    Added data of addition

---
+++ src/app/quizes/addition/data.js
@@ -0,0 +1,66 @@
+export const quiz = {
+    totalQuestions: 10,
+    questions: [
+        {
+            "id": 1,
+            "question": "52 + 32 =?",
+            "answers": ["84", "80", "78", "82"],
+            "correctAnswer": "84"
+          },
+          {
+            "id": 2,
+            "question": "40 + 17 =?",
+            "answers": ["55", "53", "57", "51"],
+            "correctAnswer": "57"
+          },
+          {
+            "id": 3,
+            "question": "13 + 8 =?",
+            "answers": ["18", "16", "20", "21"],
+            "correctAnswer": "21"
+          },
+          {
+            "id": 4,
+            "question": "8 + 5 =?",
+            "answers": ["12", "11", "10", "13"],
+            "correctAnswer": "13"
+          },
+          {
+            "id": 5,
+            "question": "29 + 21 =?",
+            "answers": ["48", "47", "49", "50"],
+            "correctAnswer": "50"
+          },
+          {
+            "id": 6,
+            "question": "25 + 14 =?",
+            "answers": ["36", "37", "38", "39"],
+            "correctAnswer": "39"
+          },
+          {
+            "id": 7,
+            "question": "15 + 6 =?",
+            "answers": ["19", "18", "21", "20"],
+            "correctAnswer": "21"
+          },
+          {
+            "id": 8,
+            "question": "15 + 9 =?",
+            "answers": ["22", "21", "24", "23"],
+            "correctAnswer": "24"
+          },
+          {
+            "id": 9,
+            "question": "25 + 16 =?",
+            "answers": ["39", "38", "41", "40"],
+            "correctAnswer": "41"
+          },
+          {
+            "id": 10,
+            "question": "12 + 8 =?",
+            "answers": ["17", "18", "19", "20"],
+            "correctAnswer": "20"
+          }
+    ],
+  };
+  
