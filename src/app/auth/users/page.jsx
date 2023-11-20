8fe1a1381e002f76a1e883740c5ef74753c9ca44
Ernesto <fierroernesto5@gmail.com>
2023-11-19 23:56:38 -0600

    Mas cambios pero aun no es responsive :) part2

--- src/app/auth/users/page.jsx
+++ src/app/auth/users/page.jsx
@@ -1,59 +1,55 @@
 'use client'
-import React, { useState } from 'react';
-import { useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
-import {useRouter} from 'next/navigation'
-
+import { useRouter } from 'next/navigation';
 
 const Page = () => {
   const [userType, setUserType] = useState(null);
   const { data: session } = useSession();
-  const router = useRouter()
-
+  const router = useRouter();
 
   useEffect(() => {
     // Verificar si hay información de sesión y si el usuario es un profesor
-    if (session?.user && session.user.isProfessor) {
-      setUserType('maestro');
+    if (session?.user && session.user.isprofessor) {
+        setUserType('maestro');
     } else {
-      setUserType('alumno');
+        setUserType('alumno');
     }
   }, [session]);
+  
 
-  return (
-    <div className="container mx-auto mt-10">
-      {userType === 'maestro' && (
-        <div className="bg-green-200 p-4 rounded-md">
-          <h1 className="text-2xl font-bold mb-2">Bienvenid@, {session?.user.name}!</h1>
-          {<div>
-            <p className="text-center">Por favor seleccione un grupo</p>
-            <div className="flex flex-row grid-cols-1">
-            <div className="bg-slate-400 justify-center items-center justify-items-center m-6 mx-32 rounded-lg shadow-lg">
-                    <div>
-                        <h1 className="text-center bg-white p-2 my-2 mx-12 rounded-lg shadow-lg">Aventura Matemática: Sumando con Diversión</h1>
-                    </div>
-                    <div className="justify-center items-center text-center">
-                        <p className="bg-white m-2 p-1 text-justify rounded-lg shadow-lg">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed accusamus quidem praesentium accusantium. Assumenda sequi eius quaerat soluta aut omnis, consequatur corrupti. Quo aliquam esse placeat beatae enim commodi tempora.
-                        </p>
-                        <a className="items-center justify-center" href="/auth/dashboard/professors">
-                            <button className="bg-orange-600 text-black text-center rounded-lg shadow-lg m-4 py-1 items-center justify-center w-3/5 hover:bg-orange-500 transition-all hover:text-white duration-500">
-                                Curso sumas
-                            </button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>}
-        </div>
-      )}
+  const renderTeacherView = () => (
+    <div className="bg-green-200 p-4 rounded-md">
+    <h1 className="text-2xl font-bold mb-2">Bienvenid@, {session?.user.name}!</h1>
+    <div>
+      <p className="text-center">Por favor seleccione un grupo</p>
+      <div className="flex flex-row grid-cols-1">
+      <div className="bg-slate-400 justify-center items-center justify-items-center m-6 mx-32 rounded-lg shadow-lg">
+              <div>
+                  <h1 className="text-center bg-white p-2 my-2 mx-12 rounded-lg shadow-lg">Aventura Matemática: Sumando con Diversión</h1>
+              </div>
+              <div className="justify-center items-center text-center">
+                  <p className="bg-white m-2 p-1 text-justify rounded-lg shadow-lg">
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed accusamus quidem praesentium accusantium. Assumenda sequi eius quaerat soluta aut omnis, consequatur corrupti. Quo aliquam esse placeat beatae enim commodi tempora.
+                  </p>
+                  <a className="items-center justify-center" href="/auth/dashboard/professors">
+                      <button className="bg-orange-600 text-black text-center rounded-lg shadow-lg m-4 py-1 items-center justify-center w-3/5 hover:bg-orange-500 transition-all hover:text-white duration-500">
+                          Curso sumas
+                      </button>
+                  </a>
+              </div>
+          </div>
+      </div>
+  </div>
+  </div>
+  );
 
-      {userType === 'alumno' && (
-        <div className="bg-green-200 p-4 rounded-md">
+  const renderStudentView = () => (
+    <div>
+       <div className="bg-green-200 p-4 rounded-md">
         <h1 className="text-2xl font-bold mb-4">Bienvenid@, {session?.user.name}!</h1>
         <p>Nos alegra tenerte de vuelta</p>
-
-        {<div>
+        <div>
             <p className="text-center">Por favor seleccione un curso</p>
             <div className="grid grid-flow-col grid-cols-2">
                 <div className="bg-slate-400 justify-center items-center justify-items-center m-6 rounded-lg shadow-lg">
@@ -88,10 +84,16 @@ const Page = () => {
                     </div>
                     </div>
             </div>
-        </div>}
+        </div>
       </div>
-      )}
-      
+    </div>
+  );
+
+  return (
+    <div className="container mx-auto mt-10">
+      {userType === 'maestro' && renderTeacherView()}
+
+      {userType === 'alumno' && renderStudentView()}
     </div>
   );
 };
