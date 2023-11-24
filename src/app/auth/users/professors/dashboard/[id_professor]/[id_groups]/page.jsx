9f2386f4fe82724bd0914a9b644d04f0051e0a9e
Ernesto <fierroernesto5@gmail.com>
2023-11-24 17:55:28 -0600

    Push casi completo, solo falta resposive de grupo

--- src/app/auth/users/professors/dashboard/[id_professor]/[id_groups]/page.jsx
+++ src/app/auth/users/professors/dashboard/[id_professor]/[id_groups]/page.jsx
@@ -14,7 +14,7 @@ async function PageGroup({ params }) {
 console.log('soy students')
 console.log(students)
   return (
-    <section className="container mx-auto h-screen">
+    <section className="container mx-auto h-screen ">
       <div className="grid grid-cols-3 gap-3 mt-10">
         {students.map((student) => (
           <StudentCard student={student} key={student.id_group}/>
